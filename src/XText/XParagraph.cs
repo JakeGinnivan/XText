f8f676ed0f5769c89d15ebbfcc66d60b29d789b7
Jake Ginnivan <jake.ginnivan@readify.net>
2014-05-08 14:39:01 +0100

    Updating NuSpec

--- src/XText/XParagraph.cs
+++ src/XText/XParagraph.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
