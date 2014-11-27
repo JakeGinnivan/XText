5894742a29a70a6e4f1a25fa989b9071ffbfc79c
Jake Ginnivan <jake@ginnivan.net>
2014-11-27 11:27:50 +0000

    Switched to newer package restore method

--- Build NuGet.cmd
+++ Build NuGet.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+%~dp0src\.nuget\nuget.exe restore %~dp0src\XText.sln
 start /wait notepad %~dp0src\XText\XText.nuspec
 %~dp0src\.nuget\nuget.exe pack %~dp0src\XText\XText.csproj -build
 
